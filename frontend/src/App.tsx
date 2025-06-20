9dc3e6385e73ce698c7da8813ba4b1d39cf47479
yusuketozaki <g2054650@gmail.com>
2025-06-20 15:38:50 +0900

    Move frontend to dedicated directory and update config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>InterroGame Frontend</h1>
+      <p>React + Vite + TypeScript テンプレート</p>
+    </div>
+  );
+}
+
+export default App;
