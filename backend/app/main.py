86c44de6e9a8bd3c60d11b1cf6beaa9a17998e5d
FukuokaKeito <keito.0o0.kirby@gmail.com>
2025-07-31 10:11:46 +0900

    Ollama APIルーターを追加し、チャット生成機能を実装

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,41 @@
-def print_hello() -> None:
-    print(
-        "Hello, World!",
-        "aaaaaaaaaadajfakjjddddddddddddddddddddddddddddddddddddddddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+from fastapi import FastAPI
+from ollama import ChatResponse, chat
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class ChatCompletionForm(BaseModel):
+    system_message: str
+    messages: dict[str, Message]
+    stream: bool = False
+    think: bool = False
+
+
+def generate_response(form_data: ChatCompletionForm):
+    model = "qwen3"
+    system_message = form_data.system_message
+
+    messages = [{"role": "system", "content": system_message}]
+
+    for message in form_data.messages.values():
+        messages.append({"role": message.role, "content": message.content})
+
+    response: ChatResponse = chat(
+        model=model,
+        messages=messages,
+        stream=form_data.stream,
+        think=form_data.think,
     )
+    return response.message.content
 
 
-print_hello()
-print(
-    "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-)
+@app.post("/v1/api/chat")
+async def response(form_data: ChatCompletionForm):
+    response = generate_response(form_data)
+    return {"message": response}
